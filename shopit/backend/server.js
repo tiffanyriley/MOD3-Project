be92ff1277e93286c3c7e2403a51b40b2ebf64c5
tiffanyriley <tiffany.riley@gmail.com>
2024-01-29 18:03:14 -0500

    included new folders

---
+++ shopit/backend/server.js
@@ -0,0 +1,14 @@
+const app = require('./app')
+
+
+const dotenv = require('dotenv');
+
+//setting up config file
+dotenv.config({path: 'backend/config/config.env'})
+
+app.listen(process.envPORT, () => { 
+    console.log(`Server started on PORT: ${process.env.PORT}' in ${process.env.NODE_ENV} model.`)
+
+})
+
+
