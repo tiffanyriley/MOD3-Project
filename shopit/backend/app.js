60fcb9a16d0f5dd80a8b4543bc84c99c33defd70
tiffanyriley <tiffany.riley@gmail.com>
2024-01-29 22:44:17 -0500

    set up routes and database.js

--- shopit/backend/app.js
+++ shopit/backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
+//Importing all of the routes
+const products = require('./routes/products');
+
+app.use('/api/v1', products)
 module.exports = app 
---
+++ shopit/backend/routes/products.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router();
+
+
+const { getProducts } = require('..controllers/productControllers')
+
+
+router.route('/products').get(getProducts)
+
+
+module.exports = router;
