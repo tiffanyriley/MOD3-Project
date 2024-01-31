ab2f6fe5c4ad9e72bedec06efe68f40995ce5a86
tiffanyriley <tiffany.riley@gmail.com>
2024-01-30 19:29:55 -0500

    set up product database

---
+++ shopit/backend/routes/product.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+
+
+const { getProducts, newProduct } = require('..controllers/productControllers')
+
+
+router.route('/products').get(getProducts);
+
+router.route('/product.new').post(newProduct);
+
+module.exports = router;
