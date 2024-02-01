b414260bb004b8a824a35ba5d90c6e7574f898ad
tiffanyriley <tiffany.riley@gmail.com>
2024-01-31 20:18:00 -0500

    update seeder file

--- shopit/backend/app.js
+++ shopit/backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 //Importing all of the routes
-const products = require('./routes/products');
+const products = require('./routes/product');
 
 app.use('/api/v1', products)
 module.exports = app 
